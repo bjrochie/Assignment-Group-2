9f122692d117498908be53d9f8ff93d0d0ee0b3e
BARRINGTON <BARRINGTON@BARRINGTON-PC>
2019-03-19 19:01:07 -0500

    update

--- SaavyTechHelpDesk/Form1.Designer.cs
+++ SaavyTechHelpDesk/Form1.Designer.cs
@@ -28,13 +28,250 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbfName = new System.Windows.Forms.TextBox();
+            this.tbphoneNum = new System.Windows.Forms.TextBox();
+            this.tblName = new System.Windows.Forms.TextBox();
+            this.tbemail = new System.Windows.Forms.TextBox();
+            this.cbrole = new System.Windows.Forms.ComboBox();
+            this.btnclearForm = new System.Windows.Forms.Button();
+            this.btnsubmit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schedulingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alertsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newJobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(125, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(420, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(420, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(125, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Role";
+            // 
+            // tbfName
+            // 
+            this.tbfName.Location = new System.Drawing.Point(228, 89);
+            this.tbfName.Name = "tbfName";
+            this.tbfName.Size = new System.Drawing.Size(158, 20);
+            this.tbfName.TabIndex = 5;
+            // 
+            // tbphoneNum
+            // 
+            this.tbphoneNum.Location = new System.Drawing.Point(553, 91);
+            this.tbphoneNum.Name = "tbphoneNum";
+            this.tbphoneNum.Size = new System.Drawing.Size(158, 20);
+            this.tbphoneNum.TabIndex = 6;
+            // 
+            // tblName
+            // 
+            this.tblName.Location = new System.Drawing.Point(228, 120);
+            this.tblName.Name = "tblName";
+            this.tblName.Size = new System.Drawing.Size(158, 20);
+            this.tblName.TabIndex = 7;
+            // 
+            // tbemail
+            // 
+            this.tbemail.Location = new System.Drawing.Point(553, 123);
+            this.tbemail.Name = "tbemail";
+            this.tbemail.Size = new System.Drawing.Size(158, 20);
+            this.tbemail.TabIndex = 8;
+            // 
+            // cbrole
+            // 
+            this.cbrole.FormattingEnabled = true;
+            this.cbrole.Location = new System.Drawing.Point(228, 154);
+            this.cbrole.Name = "cbrole";
+            this.cbrole.Size = new System.Drawing.Size(158, 21);
+            this.cbrole.TabIndex = 9;
+            // 
+            // btnclearForm
+            // 
+            this.btnclearForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclearForm.Location = new System.Drawing.Point(128, 196);
+            this.btnclearForm.Name = "btnclearForm";
+            this.btnclearForm.Size = new System.Drawing.Size(98, 29);
+            this.btnclearForm.TabIndex = 10;
+            this.btnclearForm.Text = "Clear Form";
+            this.btnclearForm.UseVisualStyleBackColor = true;
+            // 
+            // btnsubmit
+            // 
+            this.btnsubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsubmit.Location = new System.Drawing.Point(596, 196);
+            this.btnsubmit.Name = "btnsubmit";
+            this.btnsubmit.Size = new System.Drawing.Size(95, 29);
+            this.btnsubmit.TabIndex = 11;
+            this.btnsubmit.Text = "Submit";
+            this.btnsubmit.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(301, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 26);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Register Users";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userToolStripMenuItem,
+            this.newJobToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 27);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logInToolStripMenuItem,
+            this.userManagementToolStripMenuItem,
+            this.schedulingToolStripMenuItem,
+            this.alertsToolStripMenuItem});
+            this.userToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(49, 23);
+            this.userToolStripMenuItem.Text = "User";
+            // 
+            // logInToolStripMenuItem
+            // 
+            this.logInToolStripMenuItem.Name = "logInToolStripMenuItem";
+            this.logInToolStripMenuItem.Size = new System.Drawing.Size(192, 24);
+            this.logInToolStripMenuItem.Text = "Log in";
+            // 
+            // userManagementToolStripMenuItem
+            // 
+            this.userManagementToolStripMenuItem.Name = "userManagementToolStripMenuItem";
+            this.userManagementToolStripMenuItem.Size = new System.Drawing.Size(192, 24);
+            this.userManagementToolStripMenuItem.Text = "User Management";
+            // 
+            // schedulingToolStripMenuItem
+            // 
+            this.schedulingToolStripMenuItem.Name = "schedulingToolStripMenuItem";
+            this.schedulingToolStripMenuItem.Size = new System.Drawing.Size(192, 24);
+            this.schedulingToolStripMenuItem.Text = "Scheduling";
+            // 
+            // alertsToolStripMenuItem
+            // 
+            this.alertsToolStripMenuItem.Name = "alertsToolStripMenuItem";
+            this.alertsToolStripMenuItem.Size = new System.Drawing.Size(192, 24);
+            this.alertsToolStripMenuItem.Text = "Alerts";
+            // 
+            // newJobToolStripMenuItem
+            // 
+            this.newJobToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.newJobToolStripMenuItem.Name = "newJobToolStripMenuItem";
+            this.newJobToolStripMenuItem.Size = new System.Drawing.Size(62, 23);
+            this.newJobToolStripMenuItem.Text = "Report";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnsubmit);
+            this.Controls.Add(this.btnclearForm);
+            this.Controls.Add(this.cbrole);
+            this.Controls.Add(this.tbemail);
+            this.Controls.Add(this.tblName);
+            this.Controls.Add(this.tbphoneNum);
+            this.Controls.Add(this.tbfName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Register_Users";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbfName;
+        private System.Windows.Forms.TextBox tbphoneNum;
+        private System.Windows.Forms.TextBox tblName;
+        private System.Windows.Forms.TextBox tbemail;
+        private System.Windows.Forms.ComboBox cbrole;
+        private System.Windows.Forms.Button btnclearForm;
+        private System.Windows.Forms.Button btnsubmit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schedulingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alertsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newJobToolStripMenuItem;
     }
 }
 
---
+++ SaavyTechHelpDesk/Log_in.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SaavyTechHelpDesk
+{
+    partial class Log_in
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbusername = new System.Windows.Forms.TextBox();
+            this.tbpassword = new System.Windows.Forms.TextBox();
+            this.btnclearForm = new System.Windows.Forms.Button();
+            this.tbsubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(343, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Log in";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(261, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(261, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // tbusername
+            // 
+            this.tbusername.Location = new System.Drawing.Point(378, 100);
+            this.tbusername.Name = "tbusername";
+            this.tbusername.Size = new System.Drawing.Size(149, 20);
+            this.tbusername.TabIndex = 3;
+            // 
+            // tbpassword
+            // 
+            this.tbpassword.Location = new System.Drawing.Point(378, 140);
+            this.tbpassword.Name = "tbpassword";
+            this.tbpassword.Size = new System.Drawing.Size(149, 20);
+            this.tbpassword.TabIndex = 4;
+            // 
+            // btnclearForm
+            // 
+            this.btnclearForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclearForm.Location = new System.Drawing.Point(261, 191);
+            this.btnclearForm.Name = "btnclearForm";
+            this.btnclearForm.Size = new System.Drawing.Size(107, 33);
+            this.btnclearForm.TabIndex = 5;
+            this.btnclearForm.Text = "Clear Form";
+            this.btnclearForm.UseVisualStyleBackColor = true;
+            // 
+            // tbsubmit
+            // 
+            this.tbsubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbsubmit.Location = new System.Drawing.Point(420, 191);
+            this.tbsubmit.Name = "tbsubmit";
+            this.tbsubmit.Size = new System.Drawing.Size(107, 33);
+            this.tbsubmit.TabIndex = 6;
+            this.tbsubmit.Text = "Submit";
+            this.tbsubmit.UseVisualStyleBackColor = true;
+            // 
+            // Log_in
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbsubmit);
+            this.Controls.Add(this.btnclearForm);
+            this.Controls.Add(this.tbpassword);
+            this.Controls.Add(this.tbusername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Log_in";
+            this.Text = "Log_in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbusername;
+        private System.Windows.Forms.TextBox tbpassword;
+        private System.Windows.Forms.Button btnclearForm;
+        private System.Windows.Forms.Button tbsubmit;
+    }
+}
